cbb0c2b9d60ddc9984ffe325911b105fa696867b
Petre Ro <petrero@gmail.com>
2023-06-08 10:46:23 +0300

    5.2 Returning the IRI
     - And fortunately, API Platform gives us an autowireable service to do that! Before the optional argument, add a new argument type-hinted with IriConverterInterface and call it $iriConverter
    
     Then, down here, return new Response() (the one from HttpFoundation) with no content and a 204 status code
    
     The 204 means it was "successful... but there's no content to return". We'll also pass a Location header set to $iriConverter->getIriFromResource()
    
     So you can get the resource from an IRI or the IRI string from the resource, the resource being your object. Pass this $user. How nice is that?

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Api\IriConverterInterface;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,15 +11,15 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class SecurityController extends AbstractController {
 	#[Route('/login', name: 'app_login', methods: ['POST'])]
-	public function Login(#[CurrentUser] User $user = null): Response {
+	public function Login(IriConverterInterface $iriConverter, #[CurrentUser] User $user = null): Response {
 		if (!$user) {
 			return $this->json([
 				'error' => 'Invalid login request: check that the Content-Type header is "application/json"'
 			], 401);
 		}
 
-		return $this->json([
-			'user' => '/api/users/' . $user->getId()
+		return new Response(null, 204, [
+			'Location' => $iriConverter->getIriFromResource($user)
 		]);
 	}
 }
