17d19281d3eae8ed327a185d3b1004ac51529088
Petre Ro <petrero@gmail.com>
2023-06-09 21:45:49 +0300

    14.5 The ResetDatabaseTrait  - Ok, in the test environment, we're missing the database. We could easily fix this by running:
      symfony console doctrine:database:create --env=test
    
     But that's way too much work. Instead, add one more trait to our test class: use ResetDatabase
    
     This comes from Foundry: the library we've been using to create dummy fixtures via the factory classes. ResetDatabase is amazing. It automatically makes sure that the database is cleared before each test. So if you have two tests, your second test isn't going to mess up because of some data that the first test added.
    
     It's also going to create the database automatically for us. Check it out. Run
    
      symfony php bin/phpunit
     again and check out the dump. That's our response! It's our beautiful JSON-LD! We don't have any items in the collection yet, but it is working.
    
     And notice that, when we make this request, we are not sending an Accept header on the request. Remember, when we use the Swagger UI... it actually does send an Accept header that advertises that we want application/ld+json.
    
     We can add that to our test if we want. But if we pass nothing, we get JSON-LD back because that's the default format of our API.
    
     Next: let's properly finish this test, including seeding the database with data and learning about Browser's API assertions.

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -4,9 +4,11 @@ namespace Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class DragonTreasureResourceTest extends KernelTestCase {
 	use HasBrowser;
+	use ResetDatabase;
 
 	public function testGetCollectionOfTreasures(): void {
 		$this->browser()
