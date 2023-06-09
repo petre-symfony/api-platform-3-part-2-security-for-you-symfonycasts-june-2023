ef5c4f3b169ee11ffdace03ad9a1e21213528c2a
Petre Ro <petrero@gmail.com>
2023-06-09 12:38:54 +0300

    11.2 Creating our OpenApiFactory  - To create this JSON document, internally, API Platform creates an OpenApi object, populates all this data onto it and then sends it through Symfony's serializer. This is important because we can tweak the OpenApi object before it goes through the serializer. How? The OpenApi object is created via a core OpenApiFactory... and we can decorate that.
     Check it out: over in the src/ directory, create a new directory called ApiPlatform/... and inside, a new PHP class called OpenApiFactoryDecorator. Make this implement OpenApiFactoryInterface. Then go to "Code"->"Generate" or Command+N on a Mac to implement the one method we need: __invoke()
    
     Hello Service Decoration!
     - Right now, a core OpenApiFactory service exists in API Platform that creates the OpenApi object with all this data on it. Here's our sneaky plan: we're going to tell Symfony to use our new class as the OpenApiFactory instead of the core one. But... we definitely do not want to re-implement all of the core logic. To avoid that, we'll also tell Symfony to pass us the original, core OpenApiFactory.
    
     You might be familiar with what we're doing. It's class decoration: an object-oriented strategy for extending classes. It's really easy to do in Symfony and API Platform leverages it a lot.
    
     Whenever you do decoration, you will always create a constructor that accepts the interface that you're decorating. So OpenApiFactoryInterface. I'll call this $decorated. Oh, and let me put private in front of that
    
     Perfect.
    
     Down here, to start, say $openApi = $this->decorated and then call the __invoke() method passing the same argument: $context
    
     That will call the core factory which will do all the hard work of creating the full OpenApi object. Down here, return that
    
     And in between? Yup, that's where we can mess with things! To make sure this is working, for now, just dump the $openApi object
    
     The #[AsDecorator] Attribute
     - At this moment, from an object-oriented point of view, this class is set up correctly for decoration. But Symfony's container is still set up to use the normal OpenApiFactory: it's not going to use our new service at all. We somehow need to tell the container that, first, the core OpenApiFactory service should be replaced by our service, and second, that the original core service should be passed to us.
    
     How can we do that? Above the class, add an attribute called #[AsDecorator] and hit tab to add that use statement. Pass this the service id of the original, core OpenApiFactory. You can do some digging to find this or usually the documentation will tell you. API platform actually documents decorating this service, so right in their docs, you'll find that the service id is api_platform.openapi.factory
    
     That's it! Thanks to this, anyone that was previously using the core api_platform.openapi.factory service will receive our service instead. But the original one will be passed to us.
    
     So... it should be working! To test it, head to the API homepage and refresh. Yes! When this page loads, it renders the OpenAPI JSON document in the background. The dump in the web debug toolbar proves that it hit our code! And check out that beautiful OpenApi object: it has everything including security, which matches what we saw in the JSON. So now, we can tweak that!

---
+++ src/ApiPlatform/OpenApiFactoryDecorator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\OpenApi;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+
+#[AsDecorator('api_platform.openapi.factory')]
+class OpenApiFactoryDecorator implements OpenApiFactoryInterface {
+	public function __construct(private OpenApiFactoryInterface $decorated) {
+	}
+
+	public function __invoke(array $context = []): OpenApi {
+		$openApi = $this->decorated->__invoke($context);
+
+		dump($openApi);
+
+		return $openApi;
+	}
+}
