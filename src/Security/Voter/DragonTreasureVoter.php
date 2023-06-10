717158e95d7c9b67806fe2e972116296444600e1
Petre Ro <petrero@gmail.com>
2023-06-10 16:48:41 +0300

    23.4. Checking for Roles in the Voter  - What's next? Well, we don't have a test for it, but if we authenticate with an API token, in order to edit a treasure, you need to ROLE_TREASURE_EDIT, which you can get via the token scope.
     So, in the voter, we need to check if the user has that role. Add a __construct() method and autowire Security - the one from SecurityBundle - $security
    
     Then, below, before we check the owner, if not $this->security->isGranted('ROLE_TREASURE_EDIT'), then definitely return false
    
     The last test that's failing is testing that an admin can patch to edit any treasure. Because we've already injected the Security service, this is easy.
    
     Let's pretend admin users will be able to do anything. So above the switch, if $this->security->isGranted('ROLE_ADMIN'), then return true
    
     Moment of truth:
    
      symfony php bin/phpunit
     Voilà! Our logic has found a cozy home inside the voter, the security expression is now so simple it's almost scary, and we got to write our logic in PHP.
    
     Next: let's explore hiding certain fields in the response based on the user.

--- src/Security/Voter/DragonTreasureVoter.php
+++ src/Security/Voter/DragonTreasureVoter.php
@@ -3,12 +3,17 @@
 namespace App\Security\Voter;
 
 use App\Entity\DragonTreasure;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class DragonTreasureVoter extends Voter {
 	public const EDIT = 'EDIT';
+
+	public function __construct(private Security $security) {
+
+	}
 
 	protected function supports(string $attribute, mixed $subject): bool {
 		// replace with your own logic
@@ -24,11 +29,19 @@ class DragonTreasureVoter extends Voter {
 			return false;
 		}
 
+		if ($this->security->isGranted('ROLE_ADMIN')) {
+			return true;
+		}
+
 		assert($subject instanceof DragonTreasure);
 
 		// ... (check conditions and return true to grant permission) ...
 		switch ($attribute) {
 			case self::EDIT:
+				if (!$this->security->isGranted('ROLE_TREASURE_EDIT')) {
+					return false;
+				}
+
 				if ($subject->getOwner() === $user) {
 					return true;
 				}
