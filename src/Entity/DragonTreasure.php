13dad910cf98a8c6db4f6c724773f2a0032c230c
Petre Ro <petrero@gmail.com>
2023-06-10 15:36:49 +0300

    21.6. But sometimes you want to run security after the new data has been put onto the object. In that case, use an option called securityPostDenormalize. Remember denormalize is the process of taking the data and putting it onto the object. So security will still run first... and make sure we're the original owner. Now we can also say object.getOwner() == user
     That looks identical... but this time object will be the DragonTreasure with the new data. So we're checking that the new owner is also equal to the currently logged-in user.
    
     By the way, in securityPostDenormalize, you also have a previous_object variable, which is equal to the object before denormalization. So, it's identical to object up in the security option. But, we don't need that.
    
     Try the test now:
    
      symfony php bin/phpunit --filter=testPatchToUpdateTreasure
     We got it!
    
     Security vs Validation
     - This last example highlights two different types of security checks. The first check determines whether or not the user can perform this operation at all. Like: is the current user allowed to make a PATCH request to this treasure? That depends on the current user and the current DragonTreasure in the database.
    
     But the second check is saying:
    
     - Okay, now that I know I'm allowed to make a PATCH request, am I allowed to change the data in this exact way?
    
     This depends on the currently logged-in user and the data that's being sent.
    
     I'm bringing up this difference because, for me, the first case - where you're trying to figure out whether an operation is allowed at all - regardless of what data is being sent - that is a job for security. And this is exactly how I would implement it.
    
     However, the second case - where you're trying to figure out whether the user is allowed to send this exact data - like are they allowed to change the owner or not - for me, I think that's better handled by the validation layer.
    
     I'm going to keep this in the security layer right now. But later when we talk about custom validation, we'll move this into that.
    
     Up next: can we flex the security option enough to also let admin users edit anyone's treasure? Stay tuned!

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -36,7 +36,10 @@ use function Symfony\Component\String\u;
 		),
 		new GetCollection(),
 		new Post(security: 'is_granted("ROLE_TREASURE_CREATE")'),
-		new Patch(security: 'is_granted("ROLE_TREASURE_EDIT") and object.getOwner() == user'),
+		new Patch(
+			security: 'is_granted("ROLE_TREASURE_EDIT") and object.getOwner() == user',
+			securityPostDenormalize: 'object.getOwner() == user'
+		),
 		new Delete(security: 'is_granted("ROLE_ADMIN")')
 	],
 	formats: [
