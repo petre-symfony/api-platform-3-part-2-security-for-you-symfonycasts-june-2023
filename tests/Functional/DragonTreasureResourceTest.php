fc5fa404ca8a68f78d2f8da6a165a12d98ec3d3f
Petre Ro <petrero@gmail.com>
2023-06-10 10:15:05 +0300

    19.4. Testing a Token with a Bad Scope  - Let's test to make sure we don't have access if our token is missing this scope. Copy the entire test method... then paste below. Call it testPostToCreateTreasureDeniedWithoutScope().
     This time, set scopes to something else, like SCOPE_TREASURE_EDIT. Below, we now expect a 403 status code
    
     This time, let's run all the tests:
    
      symfony php bin/phpunit
     And... all green! A 422 then a 403.

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -73,4 +73,21 @@ class DragonTreasureResourceTest extends ApiTestCase {
 			->assertStatus(422)
 		;
 	}
+
+	public function testPostToCreateTreasureDeniedWithoutScope(): void{
+		$token = ApiTokenFactory::createOne([
+			'scopes' => [ ApiToken::SCOPE_TREASURE_EDIT ]
+		]);
+
+		$this->browser()
+			->post('/api/treasures', [
+				'json' => [],
+				'headers' => [
+					'Authorization' => 'Bearer ' . $token->getToken()
+				]
+			])
+			->dump()
+			->assertStatus(403)
+		;
+	}
 }
