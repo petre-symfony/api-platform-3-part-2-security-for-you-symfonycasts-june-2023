6fcd1e995f13b013e4b1f002c5546bcfdf3f4a27
Petre Ro <petrero@gmail.com>
2023-06-11 08:59:15 +0300

    28.2. Hello ContextBuilder  - Internally, API Platform has a system called a context builder, which is responsible for building the normalization or denormalization contexts that are then passed into the serializer. And, we can hook into that to change the context: like to add extra groups.
     Let's do it! Over in src/ApiPlatform/, create a new class called AdminGroupsContextBuilder... and make this implement SerializerContextBuilderInterface
    
     Then, go to "Code"->"Generate" - or Command+N on a Mac - and select "Implement methods" to create the one we need: createFromRequest()
    
     It's pretty simple: API Platform will call this, pass us the Request, whether or not we're normalizing or denormalizing... and then we return the context array that should be passed to the serializer.
    
     Let's do some Decoration!
     - Like we've seen a few times already, our intention is not to replace the core context builder. Nope, we want the core context builder to do its thing... and then we'll add our own stuff.
    
     To do this, once again, we'll use service decoration. We know how this works: add a __construct() method that accepts a private SerializerContextBuilderInterface and I'll call this $decorated
    
     Then, down here, say $context = this->decorated->createFromRequest() passing $request, $normalization and $extractedAttributes. Add a dump() to make sure this is working and return $context
    
     To tell Symfony to use our context builder in place of the real one, add our #[AsDecorator()].
    
     Here, we need the service ID of whatever the core context builder is. That's something you can find in the docs: it's api_platform.serializer.context_builder
    
     Oh, but be careful when using SerializerContextBuilderInterface: there are two of them. One of is from GraphQL: make sure you select the one from ApiPlatform\Serializer, unless you are using GraphQL.
    
     Ok! Let's see if it hits our dump! Run all of our tests: I also want to see which fail:
    
      symfony php bin/phpunit
     And... okay! We see the dump a bunch of times, followed by two failures. The first is testAdminCanPatchToEditTreasure. That's the case we're working on right now. We'll worry about testOwnerCanSeeIsPublishedFieldI in a minute.
    
     Copy the test method name and rerun that with --filter=:
    
      symfony php bin/phpunit --filter=testAdminCanPatchToEditTreasure
    
     When the Context Builder is Called
     - Perfect! We see the dump: actually three times, which is interesting. Open up that test so we can see what's going on. Yup! We're making a single PATCH request to /api/treasure/1. So, the context builder is called 3 times during just one request?
    
     It is! It's called one time when API Platform is querying and loading the DragonTreasure from the database. That's... kind of an odd situation because the context is meant to be used for the serializer... but we're simply querying for the object. But anyway, that's the first time.
    
     The next two make sense: it's called when the JSON we're sending is denormalized into the object... and a third time when the final DragonTreasure is normalized back into JSON.

---
+++ src/ApiPlatform/AdminGroupsContextBuilder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\HttpFoundation\Request;
+
+#[AsDecorator('api_platform.serializer.context_builder')]
+class AdminGroupsContextBuilder implements SerializerContextBuilderInterface {
+	public function __construct(private SerializerContextBuilderInterface $decorated) {
+	}
+
+	public function createFromRequest(Request $request, bool $normalization, array $extractedAttributes = null): array {
+		$context = $this->decorated->createFromRequest($request,$normalization, $extractedAttributes);
+
+		dump('I am working');
+
+		return $context;
+	}
+
+}
