6853916b4ad91f94e5f9dfe0dd3484e47570269a
Petre Ro <petrero@gmail.com>
2023-06-11 18:36:32 +0300

    33.1. Validating how Values Change  We still have a massive problem making sure treasures don't end up stolen! We just covered the main case: if you make a POST or a PUT request to a treasure endpoint, thanks to our new validation, we make sure you assign the owner to yourself, unless you're an admin. Yay!
     But in our API, when POSTing or PATCHing to a user endpoint, you are allowed to send a dragonTreasures field. This, unfortunately allows treasures to be stolen. Simply send a PATCH request to modify your own User record... then set the dragonTreasures field to an array containing the IRI strings of some treasures that you do not own. Whoops!
    
     The easiest solution would be to... make the field not writable. So, inside of User, for dragonTreasures, we would keep this readable, but remove the write group. That would force everyone to use the /api/treasures endpoints to manage their treasures.
    
     The Trickiness of this Problem
     - If you do want to keep the writable dragonTreasures field... you can, but this problem is tricky to solve.
    
     Let's think: if you send a dragonTreasures field that contains the IRI of a treasure you do not own, that should trigger a validation error. Ok... so maybe we add a validation constraint above this property? The problem is that, by the time that validation runs, the treasures sent over in the JSON have already been set onto this dragonTreasures property. And importantly, the owner on those treasures has already been updated to this User!
    
     Remember: when the serializer sees a DragonTreasure that is not already owned by this user, it will call addDragonTreasure()... which then calls setOwner($this). So, by the time validation runs, it's going to look like we are the owner of the treasure... even though we originally weren't!
    
     Using Previous Data?
     - What can we do? Well, API Platform does have a concept of "previous data". API Platform clones the data before deserializing the new JSON onto it, which means it is possible to get what the User object originally looked like.
    
     Unfortunately, that clone is shallow, meaning that it clones scalar fields - like username - but any objects - like the DragonTreasure objects are not cloned. There's no way via API Platform to see what they originally looked like.
    
     Testing for the Bug
     - So, we are going to solve this with validation... but with the help of a special class from Doctrine called the UnitOfWork.
    
     Alrighty, let's whip up a test to shine a light on this pesky bug. Inside tests/Functional/, open UserResourceTest. Copy the previous test, paste, and call it testTreasuresCannotBeStolen(). Create a second user with UserFactory::createOne()... and we need a DragonTreasure that we're going to try to steal. Assign its owner to $otherUser
    
     Let's do this! We log in as $user, update ourselves - which is allowed - then, for the JSON, sure, maybe we still send username... but we also send dragonTreasures set to an array with /api/treasures/ and $dragonTreasure->getId().
    
     At the bottom, assert that this returns a 422
    
     Ok! Copy the method name. We're expecting this to fail:
    
      symfony php bin/phpunit --filter=testTreasuresCannotBeStolen
    
     And... it does! Status code 200, which means we are allowing treasure to be stolen! Gasp!

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\DragonTreasureFactory;
 use App\Factory\UserFactory;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -42,6 +43,30 @@ class UserResourceTest extends ApiTestCase {
 				]
 			])
 			->assertStatus(200)
+		;
+	}
+
+	public function testTreasuresCannotBeStolen(): void {
+		$user = UserFactory::createOne();
+		$otherUser = UserFactory::createOne();
+		$dragonTreasure = DragonTreasureFactory::createOne([
+			'owner' => $otherUser
+		]);
+
+		$this->browser()
+			->actingAs($user)
+			->patch('/api/users/' . $user->getId(), [
+				'json' => [
+					'username' => 'changed',
+					'dragonTreasures' => [
+						'/api/treasures/' . $dragonTreasure->getId()
+					]
+				],
+				'headers' => [
+					'Content-Type' => 'application/merge-patch+json'
+				]
+			])
+			->assertStatus(422)
 		;
 	}
 }
