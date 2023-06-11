8aaf45aebf9debefb4e588987dc1746bd9029f5f
Petre Ro <petrero@gmail.com>
2023-06-11 17:26:52 +0300

    32.4. Checking for Ownership in the Validator  - Finally we can add our business logic. To do the owner check, we need to know who's logged in. Add a __construct() method, autowire our favorite Security class... and I'll put private in front of that, so it becomes a property
     Below, set $user = $this->security->getUser(). And if there is no user for some reason, throw a LogicException to make things explode
    
     Why not trigger a validation error? We could... but in our app, if an anonymous user is somehow successfully changing a DragonTreasure... we have some sort of misconfiguration.
    
     Finally, if $value does not equal $user - so if the owner is not the User - add that validation failure
    
     That's it! Let's try this thing!
    
      symfony php bin/phpunit --filter=testPatchToUpdateTreasure
    
     And... bingo! Whether we're creating or editing a DragonTreasure, we are not allowed to set the owner to someone that is not us.
    
     And we can add whatever other fanciness we want. Like if the user is an admin, return so that admin users are allowed to assign the owner to anyone
    
     I love this. But... there's still one big security hole: a hole that will allow a user to steal the treasures of someone else! Not cool! Let's find out what that is next and crush it.

--- src/Validator/IsValidOwnerValidator.php
+++ src/Validator/IsValidOwnerValidator.php
@@ -3,10 +3,15 @@
 namespace App\Validator;
 
 use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class IsValidOwnerValidator extends ConstraintValidator {
+	public function __construct(private Security $security)
+	{
+	}
+
 	public function validate($value, Constraint $constraint) {
 		assert($constraint instanceof IsValidOwner);
 
@@ -16,7 +21,19 @@ class IsValidOwnerValidator extends ConstraintValidator {
 
 		assert($value instanceof User);
 
-		$this->context->buildViolation($constraint->message)
-			->addViolation();
+		$user = $this->security->getUser();
+
+		if (!$user) {
+			throw new \LogicException('IsOwnerValidator should only be used when a user is logged in.');
+		}
+
+		if ($this->security->isGranted('ROLE_ADMIN')) {
+			return;
+		}
+
+		if ($value !== $user) {
+			$this->context->buildViolation($constraint->message)
+				->addViolation();
+		}
 	}
 }
