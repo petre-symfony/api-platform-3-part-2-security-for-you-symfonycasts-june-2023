1830b3691db11efb73822ceb5a458128ccacb735
Petre Ro <petrero@gmail.com>
2023-06-11 10:58:18 +0300

    30.3 When you decorate this specific normalizer, you also need to implement SerializerAwareInterface. And that's going to require you to have a setSerializer() method. Oh, let me import that use statement: I don't know why that didn't come automatically
     There we go.
    
     Inside, say, if $this->normalizer is an instanceof SerializerAwareInterface, then call $this->normalizer->setSerializer($serializer)
    
     I don't even want to get into the details of this: it just happens that the normalizer we're decorating implements another interface... so we need to also implement it.
    
     Let's try this again.
    
      symfony php bin/phpunit --filter=testOwnerCanSeeIsPublishedField
    
     Finally, we have the dump and it's failing the assertion we expect... since we haven't added the group yet. Let's do that!

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -4,9 +4,11 @@ namespace App\Normalizer;
 
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsDecorator('api_platform.jsonld.normalizer.item')]
-class AddOwnerGroupsNormalizer implements NormalizerInterface {
+class AddOwnerGroupsNormalizer implements NormalizerInterface, SerializerAwareInterface {
 	public function __construct(private NormalizerInterface $normalizer){
 
 	}
@@ -19,6 +21,12 @@ class AddOwnerGroupsNormalizer implements NormalizerInterface {
 
 	public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool {
 		return $this->normalizer->supportsNormalization($data, $format, $context);
+	}
+
+	public function setSerializer(SerializerInterface $serializer) {
+		if ($this->normalizer instanceof SerializerAwareInterface) {
+			$this->normalizer->setSerializer($serializer);
+		}
 	}
 
 }
