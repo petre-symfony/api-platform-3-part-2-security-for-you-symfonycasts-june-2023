89fa2f92b9e4c7cc6d7692526e52cbaed856b0c7
Petre Ro <petrero@gmail.com>
2023-06-11 17:23:32 +0300

    32.3 Open src/Validator/IsValidOwner.php first. This lightweight class will be used as the attribute... and it just holds options that we can configure, like $message, which is enough. Let's change the default message to something a bit more helpful
     The second class is the one that will be executed to handle the logic
    
     We'll look at that in a moment... but let's use the new constraint first. Over in DragonTreasure, down on the owner property... there we go... add the new attribute: IsValidOwner
    
     Filling in the Validator Logic
     - Now that we have this, when our object is validated, Symfony will call IsValidOwnerValidator and pass us the $value - which will be the User object - and the constraint, which will be IsValidOwner.
    
     Let's do some clean up. Remove the var and replace it with assert($constraint instanceof IsValidOwner)
    
     That's just to help my editor: we know that Symfony will always pass us that. Next, notice that it's checking to see if the  is null or blank. And if is, it does nothing. If the  property is empty, that should really be handled by a different constraint.
    
     Back in DragonTreasure, add #[Assert\NotNull]
    
     So if they forget to send owner, this will handle that validation error. Back inside our validator, if we have that situation, we can just return
    
     Below this, add one more assert() that  is an instanceof User.
    
     Really, Symfony will pass us whatever value is attached to this property... but we know that this will always be a User
    
     Finally, delete setParameter() - that's not needed in our case - and $constraint->message is reading the $message property
    
     At this point, we have a functional validator! Except... it's going to fail in all situations. Ah, let's at least make sure it's being called. Run our test:
    
      symfony php bin/phpunit --filter=testPatchToUpdateTreasure
     Beautiful failure! A 422 coming from DragonTreasureResourceTest line 110... because our constraint is never satisfied.

--- src/Validator/IsValidOwnerValidator.php
+++ src/Validator/IsValidOwnerValidator.php
@@ -2,20 +2,21 @@
 
 namespace App\Validator;
 
+use App\Entity\User;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class IsValidOwnerValidator extends ConstraintValidator {
 	public function validate($value, Constraint $constraint) {
-		/* @var App\Validator\IsValidOwner $constraint */
+		assert($constraint instanceof IsValidOwner);
 
 		if (null === $value || '' === $value) {
 			return;
 		}
 
-		// TODO: implement the validation here
+		assert($value instanceof User);
+
 		$this->context->buildViolation($constraint->message)
-			->setParameter('{{ value }}', $value)
 			->addViolation();
 	}
 }
