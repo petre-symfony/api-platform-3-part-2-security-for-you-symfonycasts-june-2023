699f73bd759e5d74513f3ee824ae274869260091
Petre Ro <petrero@gmail.com>
2023-06-11 19:00:48 +0300

    33.4. Enter UnitOfWork  - But... this will be the collection of DragonTreasure objects after they've been modified. We need to ask Doctrine what each DragonTreasure looked like when it was originally queried from the database. To do that, we need to grab an internal object from Doctrine called the UnitOfWork.
     On top, add a constructor, autowire EntityManagerInterface $entityManager... and make that's a private property.
    
     Below, grab the unit of work with $unitOfWork = $this->entityManager->getUnitOfWork()
    
     This is a powerful object that keeps track of how entity objects are changing and is responsible for knowing which objects need to be inserted, updated or deleted from the database when the entity manager flushes.
    
     Next, foreach over $value - which will be a collection - as $dragonTreasure. To help my editor, I'll assert that $dragonTreasure is an instance of DragonTreasure. And now, get the original data: $originalData = $unitOfWork->getOriginalEntityData($dragonTreasure).
    
     Pretty sweet right? Let's dd($dragonTreasure) and $originalData so we can see what they look like
    
     Go test go:
    
      symfony php bin/phpunit --filter=testTreasuresCannotBeStolen
    
     Yes! It hit the dump! And this is cool! The first part is the updated DragonTreasure object and its owner has ID 1. It's not super obvious, but $user will be id 1 and $otherUser will be id 2. So the owner was originally ID 2, but yeah: user id 1 has stolen it! Below this, we see the original data as an array. And its owner was ID 2!
    
     This info makes us dangerous.

--- src/Validator/TreasuresAllowedOwnerChangeValidator.php
+++ src/Validator/TreasuresAllowedOwnerChangeValidator.php
@@ -2,11 +2,17 @@
 
 namespace App\Validator;
 
+use App\Entity\DragonTreasure;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class TreasuresAllowedOwnerChangeValidator extends ConstraintValidator {
+	public function __construct(private EntityManagerInterface $entityManager) {
+
+	}
+
 	public function validate($value, Constraint $constraint) {
 		assert($constraint instanceof TreasuresAllowedOwnerChange);
 
@@ -15,6 +21,14 @@ class TreasuresAllowedOwnerChangeValidator extends ConstraintValidator {
 		}
 
 		assert($value instanceof Collection);
+
+		$unitOfWork = $this->entityManager->getUnitOfWork();
+		foreach ($value as $dragontreasure) {
+			assert($dragontreasure instanceof DragonTreasure);
+
+			$originalData = $unitOfWork->getOriginalEntityData($dragontreasure);
+			dd($dragontreasure, $originalData);
+		}
 
 		$this->context->buildViolation($constraint->message)
 			->setParameter('{{ value }}', $value)
