4ace04f82c278b797cd81cac5df047b04bb9373e
Petre Ro <petrero@gmail.com>
2023-06-11 18:47:16 +0300

    33.3. Next, over in src/Validator/, open up the validator class. We'll do some basic cleanup: use the assert() function to assert that $constraint is an instance of TreasuresAllowedOwnerChange. And also assert that value is an instance of Collection from Doctrine
     We know that this will be used above this property... so it will be some sort of collection of DragonTreasures.

--- src/Validator/TreasuresAllowedOwnerChangeValidator.php
+++ src/Validator/TreasuresAllowedOwnerChangeValidator.php
@@ -2,18 +2,20 @@
 
 namespace App\Validator;
 
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class TreasuresAllowedOwnerChangeValidator extends ConstraintValidator {
 	public function validate($value, Constraint $constraint) {
-		/* @var App\Validator\TreasuresAllowedOwnerChange $constraint */
+		assert($constraint instanceof TreasuresAllowedOwnerChange);
 
 		if (null === $value || '' === $value) {
 			return;
 		}
 
-		// TODO: implement the validation here
+		assert($value instanceof Collection);
+
 		$this->context->buildViolation($constraint->message)
 			->setParameter('{{ value }}', $value)
 			->addViolation();
