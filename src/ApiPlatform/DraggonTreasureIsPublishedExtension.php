618c1c8cf9ec5960dc2759126c6c9339861820d4
Petre Ro <petrero@gmail.com>
2023-06-12 09:46:03 +0300

    35.2. Collection Query Extensions  - Ok, to modify the query for a collection endpoint, we're going to create something called a query extension. Anywhere in src/ - I'll do it in the ApiPlatform/ directory - create a new class called DragonTreasureIsPublishedExtension. Make this implement QueryCollectionExtensionInterface, then go to "Code"->"Generate" or Command+N on a Mac - and generate the one method we need: applyToCollection()
     This is pretty cool: it passes us the  and a few other pieces of info. Then, we can modify that QueryBuilder. The best part? The QueryBuilder already takes into account things like pagination and any filters that have been applied. So those are not things we need to worry about.
    
     Also, thanks to Symfony's autoconfiguration system, just by creating this class and making it implement this interface, it will already be called whenever a collection endpoint is used!
    
     Query Extension Logic
     - In fact, it will be called for any resource. So the first thing we need is if (DragonTreasure::class !== $resourceClass) - fortunately it passes us the class name - then return
    
     Below, this is where we get to work. Now, every QueryBuilder object has a root alias that refers to the class or table that you're querying. Usually, we create the QueryBuilder... like from inside a repository we say something like $this->createQueryBuilder('d') and d becomes that "root alias". Then we use that in other parts of the query.
    
     However, in this situation, we didn't create the QueryBuilder, so we never chose that root alias. It was chosen for us. What is it? It's: "banana". Actually, I have no idea what it is! But we can get it with $queryBuilder->getRootAliases()[0]
    
     Now it's just normal query logic: $queryBuilder->andWhere() passing sprintf(). This looks a little weird: %s.isPublished = :isPublished, then pass $rootAlias followed by ->setParameter('isPublished', true)
    
     Cool! Spin over to try this thing!
    
      symfony php bin/console phpunit --filter=testGetCollectionOfTreasures
     Mission accomplished! It's just that easy.
    
     Query Extensions on SubResources?
     - By the way, will this also work for sub-resources? For example, over in our docs, we can also fetch a collection of treasures by going to /api/users/{user_id}/treasures. Will this also hide the unpublished treasures? The answer is... yes! So, it's not something you need to worry about. I won't show it, but this also uses the query extension.
    
     Oh, and if you wanted admin users to be able to see unpublished treasures, you could add logic to only modify this query if the current user is not an admin.
    
     Next up: this query extension fixes the collection endpoint! But... someone could still fetch a single unpublished treasure directly by its id. Let's fix that!

---
+++ src/ApiPlatform/DraggonTreasureIsPublishedExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\DragonTreasure;
+use Doctrine\ORM\QueryBuilder;
+
+class DraggonTreasureIsPublishedExtension implements QueryCollectionExtensionInterface {
+	public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void {
+		if (DragonTreasure::class !== $resourceClass) {
+			return;
+		}
+
+		$rootAlias = $queryBuilder->getRootAliases()[0];
+		$queryBuilder
+			->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+			->setParameter('isPublished', true)
+		;
+	}
+
+}
